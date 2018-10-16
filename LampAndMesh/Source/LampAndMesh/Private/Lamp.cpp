3640c532a9ab2ea549a601d6200c6fbdd12fee7e
DESKTOP-L8UUJ53\mateu <manisiek@gmail.com>
2018-10-16 14:41:29 +0200

    LAM01 created project; added new Actor class (Lamp); added new map (TestMap)

---
+++ LampAndMesh/Source/LampAndMesh/Private/Lamp.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/Lamp.h"
+#include "Components/PointLightComponent.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ALamp::ALamp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Intensity = 5000.f;
+
+	PointLight = CreateDefaultSubobject<UPointLightComponent>(FName("Point Light"));
+	PointLight->Intensity = Intensity;
+	PointLight->bVisible = true;
+	RootComponent = PointLight;
+
+	PointLightSphere = CreateDefaultSubobject<USphereComponent>(FName("Point Light Sphere"));
+	PointLightSphere->InitSphereRadius(400.f);
+	PointLightSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	PointLightSphere->AttachToComponent(PointLight, FAttachmentTransformRules::SnapToTargetIncludingScale);
+}
+
+// Called when the game starts or when spawned
+void ALamp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALamp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ LampAndMesh/Source/LampAndMesh/Public/Lamp.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Lamp.generated.h"
+
+class UPointLightComponent;
+class USphereComponent;
+
+UCLASS()
+class LAMPANDMESH_API ALamp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALamp();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Light Setup")
+		UPointLightComponent* PointLight = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Light Setup")
+		USphereComponent* PointLightSphere = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Light Setup")
+		float Intensity;
+};
